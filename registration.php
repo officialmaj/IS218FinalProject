7d0c7837d23d150e3d29fb003a88d17840fe1f93
officialmaj <sa646@njit.edu>
2017-12-22 08:19:00 -0500

    udpated registration

---
+++ registration.php
@@ -0,0 +1,40 @@
+<html>
+
+<head lang="en">
+
+	<meta charset="UTF-8">
+	<link type="text/css" rel="stylesheet" href="bootstrap-3.2.0-dist\css\bootstrap.css">
+
+	<title> Registration </title>
+
+</head>
+
+<style>
+	.login-panel {
+
+		margin-top: 150px;
+
+	}
+
+</style>
+
+
+	<body>
+
+		<div class ="container">
+			<div class="row">
+				<div class="col-md-4-md-offset-4">
+					<div class="panel-heading">
+						<h3 class="panel-title"> Registration </h3>
+					</div>
+
+
+
+		
+
+
+	</body>
+
+
+
+
